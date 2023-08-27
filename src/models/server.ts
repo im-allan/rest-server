e9cb499b587e8f15978977675946a0445f5da185
im-allan <al.silvaquiroz@gmail.com>
2023-08-26 21:08:35 -0400

    feat: the Server class is added that allows to raise this same

---
+++ src/models/server.ts
@@ -0,0 +1,55 @@
+import  express, {Express} from 'express';
+
+class Server {
+  app: Express;
+  port: string | undefined;
+  constructor () {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Middlewares
+    this.middlewares();
+    // Rutas de mi aplicación
+    this.routes();
+  }
+
+  middlewares() {
+    // Directorio público
+    this.app.use(express.static('src/public'));
+  }
+
+  routes() {
+    this.app.get('/api', (req: any, res: any) => {
+      res.json({
+        msg: 'GET API',
+      })
+    })
+
+    this.app.put('/api', (req: any, res: any) => {
+      res.status(400).json({
+        msg: 'PUT API',
+      })
+    })
+
+    this.app.post('/api', (req: any, res: any) => {
+      res.status(201).json({
+        msg: 'POST API',
+      })
+    })
+
+    this.app.delete('/api', (req: any, res: any) => {
+      res.json({
+        msg: 'DELETE API',
+      })
+    })
+  }
+
+  listen(){
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en el puerto ${this.port}!\n`)
+    })
+  }
+
+}
+
+export default Server;
