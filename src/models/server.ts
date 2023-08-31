fe1f17eca4ea41bad8206e1eb74ae539f73e3b58
im-allan <al.silvaquiroz@gmail.com>
2023-08-30 23:15:19 -0400

    feat: Added more todo's

--- src/models/server.ts
+++ src/models/server.ts
@@ -21,32 +21,35 @@ class Server {
   }
 
   routes() {
-    // TODO: buscar el tipo de dato para req y res
+    // TODO: buscar el tipo de dato para req y res 
     this.app.get('/api', (req: any, res: any) => {
       res.json({
         msg: 'GET API',
       })
     })
-
+    
+    // TODO: buscar el tipo de dato para req y res 
     this.app.put('/api', (req: any, res: any) => {
       res.status(400).json({
         msg: 'PUT API',
       })
     })
 
+    // TODO: buscar el tipo de dato para req y res 
     this.app.post('/api', (req: any, res: any) => {
       res.status(201).json({
         msg: 'POST API',
       })
     })
-
+    
+    // TODO: buscar el tipo de dato para req y res 
     this.app.delete('/api', (req: any, res: any) => {
       res.json({
         msg: 'DELETE API',
       })
     })
   }
-
+  
   listen(){
     this.app.listen(this.port, () => {
       console.log(`Servidor corriendo en el puerto ${this.port}!\n`)
