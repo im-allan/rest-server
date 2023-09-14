af2c55490b1d4d1bdb96bb0cac5918cfb66a1ec1
im-allan <al.silvaquiroz@gmail.com>
2023-09-13 21:32:56 -0300

    feat: Type of data found for parameters req res

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,24 +5,20 @@ const router = Router();
 
 module.exports = router;
 
-// TODO: buscar el tipo de dato para req y res 
 router.get('/', usersGet)
 
-// TODO: buscar el tipo de dato para req y res 
 router.put('/', (req: any, res: any) => {
   res.status(400).json({
     msg: 'PUT API',
   })
 })
 
-// TODO: buscar el tipo de dato para req y res 
 router.post('/', (req: any, res: any) => {
   res.status(201).json({
     msg: 'POST API',
   })
 })
 
-// TODO: buscar el tipo de dato para req y res 
 router.delete('/', (req: any, res: any) => {
   res.json({
     msg: 'DELETE API',
