8d17b837e404d5908d0f651ead9ce79c1080ce96
im-allan <al.silvaquiroz@gmail.com>
2023-09-13 21:34:24 -0300

    feat: Added patch router

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,17 +7,25 @@ module.exports = router;
 
 router.get('/', usersGet)
 
+router.post('/', (req: any, res: any) => {
+  res.status(201).json({
+    msg: 'POST API',
+  })
+})
+
 router.put('/', (req: any, res: any) => {
   res.status(400).json({
     msg: 'PUT API',
   })
 })
 
-router.post('/', (req: any, res: any) => {
-  res.status(201).json({
-    msg: 'POST API',
+router.patch('/', (req: any, res: any) => {
+  res.status(200).json({
+    msg: 'PUT API',
   })
 })
+
+
 
 router.delete('/', (req: any, res: any) => {
   res.json({
